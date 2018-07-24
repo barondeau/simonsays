b7a1ceaef94c81fd9d6c458d450562da730342ed
barondeau <mbaron510129@gmail.com>
2018-07-24 16:14:43 -0500

    adds colors and sounds

--- simonsays.py
+++ simonsays.py
@@ -2,6 +2,7 @@ import time
 import random
 import RPi.GPIO as GPIO
 import LEDRGB as LED
+from getpass import getpass
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 colors = ['R', 'G', 'B', 'Y']
@@ -17,14 +18,25 @@ R_pin = 11
 G_pin = 12
 B_pin = 13
 LED.setup(R_pin, G_pin, B_pin)
+
 while x==0:
+	def validate_guess(c_seq_string,guess):
+		if c_seq_string == guess:
+			print"Nice"
+		if c_seq_string != guess:
+			print"Game over man! Game Over!"
+			Buzz.ChangeFrequency(1000)
+			Buzz.start(50)
+			time.sleep(0.5)
+			Buzz.stop()
+			x=1
+			exit()
 	def loop():
 		n=random.randint(0,3)
 		n=y
 		c_seq =[colors[n]]
 		while True:
 			for color in c_seq:
-				
 				Buzz.ChangeFrequency(freq[n])
 				Buzz.start(50)
 				LED.setColor(color)
@@ -32,6 +44,9 @@ while x==0:
 				Buzz.stop()
 				LED.noColor()
 				time.sleep(0.5)
+			guess = getpass('Guess the color sequence?(r,g,b,y)')
+			c_seq_string = ''.join(c_seq)
+			validate_guess(c_seq_string,guess.upper())
 			n=random.randint(0,3)
 			c_seq.append(colors[n])
 			time.sleep(0.5)
